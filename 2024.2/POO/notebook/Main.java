c784635643bafe02e00ce9f4a7ba0572c68213c5
rudriguu2099 <eusourodrigo895@gmail.com>
2024-12-08 23:19:30 -0300

    up v2

--- 2024.2/POO/notebook/Main.java
+++ 2024.2/POO/notebook/Main.java
@@ -1,13 +1,78 @@
+class Bateria {
+    private int capacidade;
+    private int carga;
+
+    public Bateria(int capacidade){
+        setCapacidade(capacidade);
+        setCarga(capacidade);
+    }
+
+    public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
+    }
+
+    public void setCarga(int carga) {
+        if (carga <= capacidade) this.carga = carga;
+        else this.carga = capacidade;
+    }
+
+    public int getCarga() {
+        return carga;
+    }
+
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+    public void usar(int minutos){
+        if (minutos <= carga){
+            carga -= minutos;
+        } else{
+            carga = 0;
+        }
+    }
+
+    public void recarregar(){
+        carga = capacidade;
+    }
+
+    public void mostrar() {
+        System.out.println("(" + carga + "/" + capacidade + ")");
+    }
+}
+
+
 class Notebook {
     private boolean ligado;
+    private Bateria bateria;
 
     public Notebook(){
         setLigado(false);
+        setBateria(null);
+    }
+
+    public void setBateria(Bateria bateria){
+        this.bateria = bateria;
+    }
+
+    public Bateria rmBateria(){
+        Bateria antiga = this.bateria;
+        this.bateria = null;           
+        System.out.println("Bateria removida");
+        return antiga;
     }
 
     public void ligar(){
-        setLigado(true);
-        System.out.println("notebook ligado");
+        if (bateria != null && bateria.getCarga() > 0){
+            setLigado(true);
+            System.out.println("notebook ligado");
+        }
+        else if (bateria == null){
+            System.out.println("Erro: Nenhuma bateria instalada");
+        }
+        else{
+            System.out.println("Erro: Bateria sem carga");
+        }
     }
 
     public void desligar(){
@@ -16,11 +81,15 @@ class Notebook {
     }
 
     public void mostrar(){
-        if(!(getLigado())){
-            System.out.println("Status: Desligado");
+        String status = "";
+        if (ligado) status = "Ligado";
+        else status = "Desligado";
+
+        if(bateria == null){
+            System.out.println("Status: " + status + ", Bateria: Nenhuma");
         }
         else{
-            System.out.println("Status: Ligado");
+            System.out.println("Status: " + status + ", Bateria: (" + bateria.getCarga() + "/" + bateria.getCapacidade() + ")");
         }
     }
 
@@ -28,8 +97,17 @@ class Notebook {
         if (!(getLigado())){
             System.out.println("erro: ligue o notebook primeiro");
         }
+        else if (bateria == null){
+            System.out.println("Erro: Nenhuma bateria instalada");
+        }
         else{
-            System.out.println("Usando por " + tempo + " minutos");
+            bateria.usar(tempo);
+            if (bateria.getCarga() == 0) {
+                desligar();
+                System.out.println("Notebook descarregou");
+            } else {
+                System.out.println("Usando por " + tempo + " minutos");
+            }
         }
     }
 
@@ -47,11 +125,31 @@ class Notebook {
 public class Main {
     public static void main(String args[]) {
         Notebook notebook = new Notebook();
-        notebook.mostrar();  
-        notebook.usar(10);     
-        notebook.ligar();     
         notebook.mostrar();    
-        notebook.usar(10);    
-        notebook.desligar();
+        notebook.usar(10);     
+        notebook.ligar();      
+        notebook.mostrar();    
+        
+        
+        Bateria bateria = new Bateria(50);  
+        bateria.mostrar();    
+        notebook.setBateria(bateria); 
+        notebook.mostrar();    
+        
+        
+        notebook.usar(10);     
+        notebook.ligar();      
+        notebook.mostrar();    
+        notebook.usar(30);     
+        notebook.mostrar();    
+        notebook.usar(30);     
+        notebook.mostrar();    
+        
+        
+        notebook.ligar();      
+
+        
+        bateria = notebook.rmBateria(); 
+        bateria.mostrar(); 
     }
 }
